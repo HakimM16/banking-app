06342f6320b67073023f46f0b6db89c47a91b725
HakimM16 <hakimmabike2@gmail.com>
2025-07-09 08:56:51 +0100

    Created an encryption key that successfully logins the user, with a token

--- backend/banking-backend/src/main/java/com/hakimmabike/bankingbackend/services/JwtService.java
+++ backend/banking-backend/src/main/java/com/hakimmabike/bankingbackend/services/JwtService.java
@@ -2,6 +2,7 @@ package com.hakimmabike.bankingbackend.services;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -9,6 +10,8 @@ import java.util.Date;
 @Service
 public class JwtService {
     // This service will handle JWT creation and validation
+    @Value("${spring.jwt.secret}")
+    private String secret;
     public String generateToken(String email) {
         final long tokenExpiration = 86400; // Token expiration time in seconds (24 hours)
 
@@ -16,7 +19,7 @@ public class JwtService {
                 .subject(email)
                 .issuedAt(new Date())
                 .expiration(new Date(System.currentTimeMillis() + 1000 * tokenExpiration)) // Token valid for 24 hours
-                .signWith(Keys.hmacShaKeyFor("secret".getBytes()))
+                .signWith(Keys.hmacShaKeyFor(secret.getBytes()))
                 .compact();
     }
 
