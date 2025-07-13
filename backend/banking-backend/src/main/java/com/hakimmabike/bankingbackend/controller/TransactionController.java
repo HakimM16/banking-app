3bd077b2dd1e9fd063cf8807f37a87c5f9475863
HakimM16 <hakimmabike2@gmail.com>
2025-07-13 22:31:36 +0100

    Added repsonse as not found to getTransactionCategory()

--- backend/banking-backend/src/main/java/com/hakimmabike/bankingbackend/controller/TransactionController.java
+++ backend/banking-backend/src/main/java/com/hakimmabike/bankingbackend/controller/TransactionController.java
@@ -200,50 +200,6 @@ public class TransactionController {
         return ResponseEntity.ok(transactions);
     }
 
-    // Create a new transaction category
-    @PostMapping("/category")
-    public ResponseEntity<?> createTransactionCategory(@RequestBody CreateTransactionCategoryRequest request) {
-        // Check if name is provided
-        if (request.getName() == null || request.getName().isEmpty()) {
-            return ResponseEntity.badRequest().body("Name can't be empty"); // Return 400 Bad Request if name is missing
-        }
 
-        // check if category exists
-        if (categoryRepository.existsByName(request.getName())) {
-            return ResponseEntity.status(409).body("Category already exists"); // Return 409 Conflict if category already exists
-        }
 
-        // check if category type is valid
-        if (!TransactionType.isValidCategoryType(request.getCategoryType())) {
-            return ResponseEntity.badRequest().body("Category type is invalid"); // Return 400 Bad Request if category type is missing
-        }
-        // Create a new transaction category
-        TransactionCategoryDto createdCategory = transactionService.createTransactionCategory(request);
-        // Return the created category with a 201 Created status
-        return ResponseEntity.status(201).body(createdCategory);
-    }
-
-    // Get a specific transaction category by name
-    @GetMapping("/category/{name}")
-    public ResponseEntity<TransactionCategoryDto> getTransactionCategory(@RequestBody GetTransactionCategoryRequest request) {
-        // Fetch the transaction category by name
-        TransactionCategoryDto category = transactionService.getTransactionCategory(request.getName());
-
-        // If the category is not found, return a 404 Not Found status
-        if (category == null) {
-            return ResponseEntity.notFound().build();
-        }
-
-        // Return the category with a 200 OK status
-        return ResponseEntity.ok(category);
-    }
-
-    // Get all transaction categories
-    @GetMapping("/categories")
-    public ResponseEntity<List<TransactionCategoryDto>> getAllTransactionCategories() {
-        // Fetch all transaction categories
-        List<TransactionCategoryDto> categories = transactionService.getAllTransactionCategories();
-        // Return the list of categories with a 200 OK status
-        return ResponseEntity.ok(categories);
-    }
 }
