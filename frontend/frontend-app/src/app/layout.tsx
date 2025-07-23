44b55819b25dbf0835b645008c86370763b3e1d1
HakimM16 <hakimmabike2@gmail.com>
2025-07-23 08:20:37 +0100

    Changed the font style to Ubuntu

--- frontend/frontend-app/src/app/layout.tsx
+++ frontend/frontend-app/src/app/layout.tsx
@@ -3,10 +3,7 @@ import '@/styles/globals.css'; // Global styles for Tailwind CSS
 import type { Metadata } from 'next'; // Import Metadata type
 import { ClientAuthProvider } from '@/providers/ClientAuthProvider';
 
-export const metadata: Metadata = {
-    title: 'Vesta',
-    description: 'Your secure online banking platform',
-};
+
 
 export default function RootLayout({
     children,
@@ -15,10 +12,17 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
+            <head>
+                <link rel="preconnect" href="https://fonts.googleapis.com"/>
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap"
+                    rel="stylesheet"/>
+            </head>
             <body>
-                <ClientAuthProvider>
-                    {children}
-                </ClientAuthProvider>
+            <ClientAuthProvider>
+                {children}
+            </ClientAuthProvider>
             </body>
         </html>
     );
