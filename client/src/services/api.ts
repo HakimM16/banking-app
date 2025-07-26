67d2a5dcee5174fafd8fde8fc48784d6061bf3dc
HakimM16 <hakimmabike2@gmail.com>
2025-07-26 20:57:39 +0100

    Added .env files

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -26,7 +26,7 @@ import {Decimal} from "decimal.js"; // Or just use native fetch
 // In this file, we define the API functions that interact with the backend (Basically the endpoints we created in the backend).
 
 // Define the base URL for your API
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080';
 
 // API functions for interacting with the backend endpoints
     export const api = {
@@ -34,7 +34,7 @@ const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
         // User login
         login: async (data: LoginFormInputs) => {
-            const response = await axios.post(`${API_BASE_URL}/auth/login`, data);
+            const response = await axios.post(`${API_BASE_URL}/api/auth/login`, data);
             console.log(`Response data: email=${data.email}, password=${data.password}`);
             if (response.data.token) {
                 // Store the token in localStorage (if needed)
@@ -48,7 +48,7 @@ const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
         // User registration
         register: async (data: RegisterFormInputs) => {
-            const response = await axios.post(`${API_BASE_URL}/auth/register`, data);
+            const response = await axios.post(`${API_BASE_URL}/api/auth/register`, data);
             console.log(`Response data: email=${data.email}, password=${data.password}`);
             return response.data;
         },
@@ -57,36 +57,36 @@ const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
         // Get user profile by user ID
         getUserProfile: async (userId: number) => {
-            const response = await axios.get<User>(`${API_BASE_URL}/user/${userId}`);
+            const response = await axios.get<User>(`${API_BASE_URL}/api/user/${userId}`);
             return response.data;
         },
 
         // Update user profile
         updateUserProfile: async (userId: number, data: UpdateUserFormInputs) => {
-            const response = await axios.put<User>(`${API_BASE_URL}/user/${userId}`, data);
+            const response = await axios.put<User>(`${API_BASE_URL}/api/user/${userId}`, data);
             return response.data;
         },
 
         // Create a new address for the user
         createUserAddress: async (userId: number, data: CustomiseAddressFormInputs) => {
-            const response = await axios.post<Address>(`${API_BASE_URL}/user/${userId}/create_address`, data);
+            const response = await axios.post<Address>(`${API_BASE_URL}/api/user/${userId}/create_address`, data);
             return response.data;
         },
 
         // Update user's address
         updateUserAddress: async (userId: number, data: CustomiseAddressFormInputs) => {
-            const response = await axios.put<Address>(`${API_BASE_URL}/user/${userId}/update_address`, data);
+            const response = await axios.put<Address>(`${API_BASE_URL}/api/user/${userId}/update_address`, data);
             return response.data;
         },
 
         // Get user's address
         getUserAddress: async (userId: number) => {
-            const response = await axios.get<Address>(`${API_BASE_URL}/user/${userId}/address`);
+            const response = await axios.get<Address>(`${API_BASE_URL}/api/user/${userId}/address`);
             return response.data;
         },
 
         emailExists: async (email: string) => {
-            const response = await axios.get<Boolean>(`${API_BASE_URL}/user/email_exists/${email}`);
+            const response = await axios.get<Boolean>(`${API_BASE_URL}/api/user/email_exists/${email}`);
             return response.data;
         },
 
@@ -94,37 +94,37 @@ const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
         // Get all accounts for a user
         getAccounts: async (userId: number) => {
-            const response = await axios.get<Account[]>(`${API_BASE_URL}/accounts/user/${userId}`);
+            const response = await axios.get<Account[]>(`${API_BASE_URL}/api/accounts/user/${userId}`);
             return response.data;
         },
 
         // Get count of active accounts for a user
         getActiveAccounts: async (userId: number): Promise<AccountCount> => {
-            const response: AxiosResponse<AccountCount> = await axios.get<AccountCount>(`${API_BASE_URL}/accounts/${userId}/active-accounts`);
+            const response: AxiosResponse<AccountCount> = await axios.get<AccountCount>(`${API_BASE_URL}/api/accounts/${userId}/active-accounts`);
             return response.data;
         },
 
         // Get total balance for a user
         getBalance: async (userId: number) => {
-            const response = await axios.get<Decimal>(`${API_BASE_URL}/accounts/${userId}/total-balance`);
+            const response = await axios.get<Decimal>(`${API_BASE_URL}/api/accounts/${userId}/total-balance`);
             return response.data;
         },
 
         // Get balance for account
         getAccountBalance: async (userId: number, accountId: number) => {
-            const response = await axios.get<Decimal>(`${API_BASE_URL}/accounts/${userId}/${accountId}/balance`);
+            const response = await axios.get<Decimal>(`${API_BASE_URL}/api/accounts/${userId}/${accountId}/balance`);
             return response.data;
         },
 
         // Create a new account for a user
         createAccount: async (userId: number, data: CreateAccountFormInputs) => {
-            const response = await axios.post<Account>(`${API_BASE_URL}/accounts/${userId}`, data);
+            const response = await axios.post<Account>(`${API_BASE_URL}/api/accounts/${userId}`, data);
             return response.data;
         },
 
         // Update account status
         updateAccountStatus: async (userId: number, accountId: number, data: UpdateAccountStatusFormInputs) => {
-            const response = await axios.patch<Account>(`${API_BASE_URL}/accounts/${userId}/${accountId}/status`, data);
+            const response = await axios.patch<Account>(`${API_BASE_URL}/api/accounts/${userId}/${accountId}/status`, data);
             return response.data;
         },
 
@@ -132,31 +132,31 @@ const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
         // Deposit funds into user's account
         deposit : async (userId: number, data: DepositFormInputs) => {
-            const response = await axios.post<Transaction>(`${API_BASE_URL}/transactions/${userId}/deposit`, data);
+            const response = await axios.post<Transaction>(`${API_BASE_URL}/api/transactions/${userId}/deposit`, data);
             return response.data;
         },
 
         // Withdraw funds from user's account
         withdraw: async (userId: number, data: WithdrawFormInputs) => {
-            const response = await axios.post<Transaction>(`${API_BASE_URL}/transactions/${userId}/withdraw`, data);
+            const response = await axios.post<Transaction>(`${API_BASE_URL}/api/transactions/${userId}/withdraw`, data);
             return response.data;
         },
 
         // Transfer funds between accounts
         transfer: async (userId: number, data: TransferFormInputs) => {
-            const response = await axios.post<Transaction>(`${API_BASE_URL}/transactions/${userId}/transfer`, data);
+            const response = await axios.post<Transaction>(`${API_BASE_URL}/api/transactions/${userId}/transfer`, data);
             return response.data;
         },
 
         // Get all transactions for a user's account
         getTransactions: async (userId: number) => {
-            const response = await axios.get<Transaction[]>(`${API_BASE_URL}/transactions/${userId}/account`);
+            const response = await axios.get<Transaction[]>(`${API_BASE_URL}/api/transactions/${userId}/account`);
             return response.data;
         },
 
         // Get total count of transactions for a user
         getTransactionsCount: async (userId: number) => {
-            const response = await axios.get<TransactionCount>(`${API_BASE_URL}/transactions/${userId}/total`);
+            const response = await axios.get<TransactionCount>(`${API_BASE_URL}/api/transactions/${userId}/total`);
             return response.data;
         },
 
