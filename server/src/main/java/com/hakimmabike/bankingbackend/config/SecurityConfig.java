1fef796aef15dcafe967a5fa84c84bf115acc39f
HakimM16 <hakimmabike2@gmail.com>
2025-07-28 14:07:17 +0100

    Testing for depositForm

--- server/src/main/java/com/hakimmabike/bankingbackend/config/SecurityConfig.java
+++ server/src/main/java/com/hakimmabike/bankingbackend/config/SecurityConfig.java
@@ -74,8 +74,9 @@ public class SecurityConfig {
 
                             // Allow unauthenticated access to the health endpoint
                             c.requestMatchers("/actuator/health").permitAll();
+                            c.requestMatchers("/api/transactions/**").permitAll();
                             // Apply feature-specific security rules first
-//                            featureSecurityRules.forEach(r -> r.configure(c));
+                            featureSecurityRules.forEach(r -> r.configure(c));
                             // Then apply the catch-all authenticated rule
                             c.anyRequest().authenticated();
                         }
